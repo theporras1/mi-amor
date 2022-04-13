44a6540083fd685d4d10280cfc9534bffa6e75e2
theporras1 <94507786+theporras1@users.noreply.github.com>
2022-04-13 14:48:37 -0600

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,17 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('SABIA QUE DIRIAS SI')
+    alert('ME HACES LA PERSONA MAS FELIZ')
+    alert('ME GUSTA CADA MOMENTO QUE PASO CONTIGO')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
